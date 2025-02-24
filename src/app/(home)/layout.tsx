60195c62896f779285f2b592c3d68a44cdbd19dc
Luis Javier Castillo <t1053300321@unitru.edu.pe>
2025-02-23 20:06:28 -0500

    diference between layout and template

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -2,6 +2,7 @@ import { Hero } from "@/components/home/Hero/Hero";
 import { Description } from "@/components/home/Description";
 
 export default function HomeLayout ({children}: Readonly<{children: React.ReactNode}>){
+
     return(
         <div>
             <main>
